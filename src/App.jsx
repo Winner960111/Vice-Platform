d9f5936f3b584955510df7a78f02e59917a79148
Winner960111 <chenyih0111@gmail.com>
2025-02-18 13:59:22 +0900

    Initiate(master):first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {Routes, Route} from "react-router-dom";
+import LandingPage from "./Pages/LandingPage.jsx";
+function App() {
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LandingPage/>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "../Components/Header.jsx";
+const LandingPage = () => {
+  return (
+    <div>
+        <Header/>
+    </div>
+  );
+};
+export default LandingPage;
